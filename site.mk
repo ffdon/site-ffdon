9c4d191a3c672800241022cf536d193b5fceb057
ff-mgk <freifunk@growngizmo.de>
2016-03-30 18:23:31 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -36,7 +36,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6.1+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.6.1
 
 
 ##	GLUON_RELEASE
